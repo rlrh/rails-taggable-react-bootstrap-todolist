68b2f9f16a727c034db2811fa93950042447da97
Rayner Lim <rlrh1996@gmail.com>
2018-12-13 22:04:10 +0800

    Main React component created

---
+++ app/assets/javascripts/components/_main.js.jsx
@@ -0,0 +1,7 @@
+const Main = (props) => {
+    return(
+      <div>
+        <h1>To-do lists are great!</h1>
+      </div>
+    )
+}
---
+++ app/controllers/api/todos_controller.rb
@@ -0,0 +1,27 @@
+class Api::TodosController < ApplicationController
+    def index
+      render json: Todo.all
+    end
+  
+    def create
+      todo = Todo.create(todo_params)
+      render json: todo
+    end
+  
+    def destroy
+      Todo.destroy(params[:id])
+    end
+  
+    def update
+      todo = Todo.find(params[:id])
+      todo.update_attributes(todo_params)
+      render json: todo
+    end
+  
+    private
+  
+    def todo_params
+      params.require(:todo).permit(:id, :completed, :description)
+    end
+  end
+  
